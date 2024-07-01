29bb667ffa044bcc02c591d6e10542adaac9cdf3
theNightBirde <44029916+KINGSMITH97@users.noreply.github.com>
2024-07-01 12:12:06 +0000

    initial commit

---
+++ variable.tf
@@ -0,0 +1,9 @@
+
+variable "bucket_name" {
+  default = "s3website302024"
+}
+
+variable "website_path" {
+  type    = string
+  default = "/home/thenightbirde/Desktop/tourist-attraction-website/"
+}
